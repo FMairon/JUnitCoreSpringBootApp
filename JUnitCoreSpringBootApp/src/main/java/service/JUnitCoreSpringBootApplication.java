076b84075a6aff4443bb8610bfd5b8847fa0121f
D060586 <felix.mairon@sap.com>
2018-03-22 10:50:22 +0000

    inital commit

---
+++ JUnitCoreSpringBootApp/src/main/java/service/JUnitCoreSpringBootApplication.java
@@ -0,0 +1,19 @@
+package service;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitCoreSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JUnitCoreSpringBootApplication.class, args);
+
+    JUnitCore jUnitCore = new JUnitCore();
+
+    jUnitCore = new JUnitCore();
+    Result result = jUnitCore.run(SomeTests.class);
+  }
+}
---
+++ JUnitCoreSpringBootApp/src/main/java/service/SomeTests.java
@@ -0,0 +1,24 @@
+package service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SomeTests {
+
+  @Test
+  public void testSuccess() throws Exception {
+    assertTrue(true);
+  }
+
+  @Test
+  public void testFail() throws Exception {
+    assertTrue(false);
+  }
+
+}
